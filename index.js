86a1880625cda6765e6af1dd1eaada78841b9efc
Alvin Li <aflip24@gmail.com>
2023-03-16 04:12:11 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400){
+    return 'This one is on me!';
+  } else if(someNumber >= 400, someNumber <= 2000){
+    return 'That will be twenty bucks.';
+  } else if(someNumber >= 2000, someNumber <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else if(someNumber > 2500){
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  if (someCity === 'NYC'){
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+  switch(someTip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+ 
 }
